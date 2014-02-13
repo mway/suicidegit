e349d3bfb0a704b576e1faa04408c5f81165200c
Matt Way <io@mway.me>
2014-02-13 02:46:08 -0500

    copy instead of linking

--- install-bash.sh
+++ install-bash.sh
@@ -3,7 +3,7 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
  
 chmod +x $DIR/sg.sh
-sudo ln -s $DIR/sg.sh /usr/local/bin/sg
+sudo cp $DIR/sg.sh /usr/local/bin/sg
 _alias="alias git=sg"
 $_alias
 echo $_alias > ~/.bashrc
